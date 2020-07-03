596cce28d03fc07022b686722f2eefe94fd51a37
Lucas Baragatti <lucas@edf.etc.br>
2020-07-03 19:50:35 -0300

    Corrige formatação na barra de ações

--- src/components/BarraAcoes.tsx
+++ src/components/BarraAcoes.tsx
@@ -1,5 +1,5 @@
 import React, {useCallback, useEffect, useLayoutEffect, useState} from 'react';
-import {Card, CardContent, Theme} from '@material-ui/core';
+import {Card, CardContent} from '@material-ui/core';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import {drawerWidth} from '../client/main/Menu';
 import {useDebouncer} from '../common/Debouncer';
@@ -7,7 +7,7 @@ import {useDebouncer} from '../common/Debouncer';
 const alturaBarra = 52;
 const larguraMaxima = 960;
 
-const useStyles = makeStyles((theme: Theme) => ({
+const useStyles = makeStyles(() => ({
   wrapper: {
     position: 'absolute',
     display: 'flex',
@@ -15,9 +15,6 @@ const useStyles = makeStyles((theme: Theme) => ({
     zIndex: 999,
     height: alturaBarra + 16,
     padding: 8,
-    [theme.breakpoints.down('sm')]: {
-      width: '100%',
-    },
   },
   card: {
     width: '100%',
