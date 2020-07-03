2fc7e8b0d40af577945ea27f4bd7f5a627ed5bde
Lucas Baragatti <lucas@edf.etc.br>
2020-07-03 19:48:42 -0300

    Corrige formação do ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,41 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
-  },
-  'extends': [
-    'plugin:react/recommended',
-    'google',
-  ],
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly',
-  },
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
-    },
-    'ecmaVersion': 2018,
-    'sourceType': 'module',
-  },
-  'plugins': [
-    'react',
-    '@typescript-eslint',
-  ],
-  'rules': {
-    'require-jsdoc': 0,
-    'no-invalid-this': 0,
-    'semi': 'off',
-    '@typescript-eslint/semi': ['error'],
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
-    'max-len': [2, {
-      'code': 120,
-      'tabWidth': 2,
-      'ignoreUrls': true,
-      'ignorePattern': '^import [^,]+ from |^export | implements',
-    }],
-  },
+	'env': {
+		'browser': true,
+		'es6': true,
+	},
+	'extends': [
+		'plugin:react/recommended',
+		'google',
+	],
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly',
+	},
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true,
+		},
+		'ecmaVersion': 2018,
+		'sourceType': 'module',
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint',
+	],
+	'rules': {
+		'require-jsdoc': 0,
+		'no-invalid-this': 0,
+		'quote-props': 0,
+		'semi': 'off',
+		'@typescript-eslint/semi': ['error'],
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': 'error',
+		'max-len': [2, {
+			'code': 120,
+			'tabWidth': 2,
+			'ignoreUrls': true,
+			'ignorePattern': '^import [^,]+ from |^export | implements',
+		}],
+	},
 };
