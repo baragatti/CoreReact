c8afeac71e798daa742322f1097a76f1e1bb271c
Lucas Baragatti <lucas@edf.etc.br>
2020-07-06 18:21:51 -0300

    Corrige responsivo da tela de cadastro de rotas

--- src/client/rotas/cadastro/CadastroRotasView.tsx
+++ src/client/rotas/cadastro/CadastroRotasView.tsx
@@ -47,12 +47,12 @@ function CadastroRotasView(props: Props) {
           <CardContent>
             <Formulario<Rota> objeto={rota} erros={erros}>
               <Grid container>
-                <Grid item md={6} xs={12}>
+                <Grid item xs={12} sm={6}>
                   <CampoFormulario propriedade="NOME">
                     <TextField label="Nome"/>
                   </CampoFormulario>
                 </Grid>
-                <Grid item md={6} xs={12}>
+                <Grid item xs={12} sm={6}>
                   <CampoFormulario propriedade="OBSERVACAO">
                     <TextField label="Observação"/>
                   </CampoFormulario>
